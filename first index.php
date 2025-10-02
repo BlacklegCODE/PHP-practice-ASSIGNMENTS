8daf2abc5b959c480c45a6c2bf46a78be79c8684
Harsh Raundal <dkingston0000@gmail.com>
2025-10-02 20:11:24 +0530

    Create first index.php

---
+++ first index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Harsh";
+        
+        $a = 5;
+        $b = 6.6;
+        echo $a + $b;
+        
+        $bro = "This is a string";
+        var_dump($bro);
+        var_dump($a);
+        var_dump($b);
+        
+        
+        echo $bro;
+        
+        ?>
+        
+    </body>
+</html>
