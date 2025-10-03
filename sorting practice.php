dd598b240a33628fbc986df72b8334a4fda24b88
Harsh Raundal <dkingston0000@gmail.com>
2025-10-03 21:52:28 +0530

    Add PHP script for sorting and basic functions
    The script includes various PHP functionalities such as sorting arrays, defining functions, and using control structures. It demonstrates both ascending and descending sorting of arrays.

---
+++ sorting practice.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        /*
+        echo "Harsh";
+        
+        $a = 5;
+        $b = 6.6;
+        echo $a + $b;
+        
+        $bro = "This is a string";
+        var_dump($bro);
+        var_dump($a);
+        var_dump($b);
+        */
+        
+        /*
+        $fruits = array('apple','bro','doctor');
+        var_dump($fruits);
+        
+        $var1 = null;
+        */
+        
+//        $str1 = "The two time back to back harsh";
+//        echo strlen($str1),"<br>";
+//        echo str_word_count($str1),"<br>";
+//        echo strrev($str1),"<br>";
+//        echo strpos($str1, 'is'),"<br>";
+//        echo str_replace('time', 'bro', $str1),"<br>";
+//        echo $str1;    
+        
+        
+//        define("PI", 3.14);
+//        echo PI,"<br>";
+//        
+//        #Operators :
+//        
+//        echo "Value of 3 + 4 is : ",3+4,"<br>";
+//        echo "Value of 3 - 4 is : ",3-4,"<br>";
+//        echo "Value of 3 * 4 is : ",3*4,"<br>";
+//        echo "Value of 3 ** 4 is : ",3**4,"<br>";
+//        echo "Value of 3 / 4 is : ",3/4,"<br>";
+//        echo "Value of 3 % 4 is : ",3%4,"<br>";
+//        $x = 4;
+//        echo $x,"<br>";
+//        $x = $x + 1;
+//        $x++;
+//        echo $x,"<br>";
+//        $x--;
+//        echo $x;
+//        
+        
+//        $t = date("H");
+//        
+//        if($t <"30")
+//        {
+//            echo $t . " is a good time","<br>";
+//        }
+//        elseif ($t == 10) {
+//            echo "Time is 10";
+//        }
+//        else
+//        {
+//            echo "Bad luck";
+//        }
+//        
+    
+//        while loop
+//        $var2 = 1;
+//        while( $var2 <= 4)
+//        {
+//            if($var2 == 3)
+//            {
+//                $var2++;
+//                continue;
+//            }
+//            echo "The current value of var is ". $var2."<br>";
+//            $var2 ++;
+//        }
+//        echo "next style ","<br>";
+//        $var3 = 1;
+//        while($var3 < 10)
+//        {
+//            if($var3 == 5)
+//            {
+//                $var3 ++;
+//                break;
+//            }
+//            echo "Number :". $var3 . "<br>";
+//            $var3 ++;
+//        }
+//        
+//        for($i = 0; $i<5;$i++)
+//        {
+//            echo 'current :'.$i."<br>";
+//        }
+//        $num = 1;
+//        do
+//        {
+//            echo "This is do while loop iteration number :".$num."<br>";
+//            $num ++;
+//        }while($num <= 6)
+        
+//        function avg_no($num1, $num2)
+//        {
+//            $avg = ($num1 + $num2)/2;
+//            return $avg;
+//        }
+//        echo avg_no(5,8);
+//        
+//        function area($a, $b)
+//        {
+//            $ar = $a * $b;
+//            return "area is " . $ar;
+//        }
+//        echo area(4,5);
+        
+        $num = array(3,5,7,3,2,5,9,8,7,4);
+        $rn = array(4,2,6,8,6,66,77,33,11,2,3,9,5,0,66,4,22,778,2);
+        
+        rsort($rn);
+        sort($num);
+        $len = count($num);
+        
+        for($i = 0;$i<=$len;$i++)
+        {
+            echo $num[$i];
+            echo"<br>";
+        }
+        echo 'reverse :'."<br>";
+        $l = count($rn);
+        for($i = 0;$i <= $l;$i++)
+        {
+        echo $rn[$i];
+        echo '<br>';
+        }
+           
+        ?>
+        
+    </body>
+</html>
