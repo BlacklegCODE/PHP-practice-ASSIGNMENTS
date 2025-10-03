34c418346b5121ca8e8a3d85e93c5b1e0b09724e
Harsh Raundal <dkingston0000@gmail.com>
2025-10-03 08:21:55 +0530

    Create operators practice.php

---
+++ operators practice.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        /*
+        echo "Harsh";
+        
+        $a = 5;
+        $b = 6.6;
+        echo $a + $b;
+        
+        $bro = "This is a string";
+        var_dump($bro);
+        var_dump($a);
+        var_dump($b);
+        */
+        
+        /*
+        $fruits = array('apple','bro','doctor');
+        var_dump($fruits);
+        
+        $var1 = null;
+        */
+        
+//        $str1 = "The two time back to back harsh";
+//        echo strlen($str1),"<br>";
+//        echo str_word_count($str1),"<br>";
+//        echo strrev($str1),"<br>";
+//        echo strpos($str1, 'is'),"<br>";
+//        echo str_replace('time', 'bro', $str1),"<br>";
+//        echo $str1;    
+        
+        
+        define("PI", 3.14);
+        echo PI,"<br>";
+        
+        #Operators :
+        
+        echo "Value of 3 + 4 is : ",3+4,"<br>";
+        echo "Value of 3 - 4 is : ",3-4,"<br>";
+        echo "Value of 3 * 4 is : ",3*4,"<br>";
+        echo "Value of 3 ** 4 is : ",3**4,"<br>";
+        echo "Value of 3 / 4 is : ",3/4,"<br>";
+        echo "Value of 3 % 4 is : ",3%4,"<br>";
+        $x = 4;
+        echo $x,"<br>";
+        $x = $x + 1;
+        $x++;
+        echo $x,"<br>";
+        $x--;
+        echo $x;
+        
+        
+        ?>
+        
+    </body>
+</html>
